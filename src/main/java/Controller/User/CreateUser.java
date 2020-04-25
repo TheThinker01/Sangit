36f38617839f01e054c3cee35555565cb1b869ea
TheThinker01 <archisman.pathak@iitkgp.ac.in>
2020-04-25 12:05:57 +0530

    File Upload Facility added

---
+++ src/main/java/Controller/User/CreateUser.java
@@ -0,0 +1,78 @@
+package Controller.User;
+
+import Bean.User;
+import DAO.UserDeclaration;
+import DAO.UserInterface;
+import HelpingClasses.BCrypt;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.File;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/addUser")
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 10, maxFileSize = 1024 * 1024 * 30, maxRequestSize = 1024 * 1024 * 50)
+public class CreateUser extends HttpServlet {
+    public static final String UPLOAD_DIR = "uploadedFiles";
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/User/register.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String name,password,email,mobile_num,category,username;
+        String photopath="";
+        String applicationPath = getServletContext().getRealPath(""),
+                uploadPath = applicationPath + File.separator + UPLOAD_DIR;
+
+        File fileUploadDirectory = new File(uploadPath);
+        if (!fileUploadDirectory.exists()) {
+            fileUploadDirectory.mkdirs();
+        }
+
+        String fileName = "";
+        for (Part part : req.getParts()) {
+            fileName = extractFileName(part);
+
+            try {
+                part.write(uploadPath + File.separator + fileName);
+                photopath = uploadPath + File.separator + fileName;
+                System.out.println("Upload Success");
+            } catch (IOException ioObj) {
+                photopath="";
+                System.out.println("Failure : " + ioObj.getMessage());
+            }
+        }
+
+        name = req.getParameter("name");
+        password = req.getParameter("password");
+        email = req.getParameter("email");
+        mobile_num = req.getParameter("mobile_num");
+        category = req.getParameter("category");
+        username = req.getParameter("username");
+
+        password = BCrypt.hashpw(password,BCrypt.gensalt(12));
+
+        UserInterface ui = new UserDeclaration();
+        ui.userInsert(new User(name,  password,  email,  mobile_num, category, photopath , username));
+
+    }
+    private String extractFileName(Part part) {
+        String fileName = "",
+                contentDisposition = part.getHeader("content-disposition");
+        String[] items = contentDisposition.split(";");
+        for (String item : items) {
+            if (item.trim().startsWith("filename")) {
+                fileName = item.substring(item.indexOf("=") + 2, item.length() - 1);
+            }
+        }
+        return fileName;
+    }
+}
--- src/main/webapp/WEB-INF/User/register.jsp
+++ src/main/webapp/WEB-INF/User/register.jsp
@@ -1,16 +1,50 @@
 <%--
   Created by IntelliJ IDEA.
   User: archi
-  Date: 24-04-2020
-  Time: 20:41
+  Date: 01-04-2020
+  Time: 18:21
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Add Details</title>
+    <%@include file="../common/commonlinks.jsp"%>
 </head>
 <body>
 
+<div class="container">
+    <form method="post" action="/addUser" enctype="multipart/form-data" >
+        <fieldset class="form-group">
+            <label>Name</label>
+            <input type="text" name="name" value="${st.getName()}" class="form-control" required/>
+        </fieldset>
+        <fieldset class="form-group">
+            <label>Category</label>
+            <input type="text" name="category" value="${st.getCategory()}" class="form-control" required/>
+        </fieldset>
+        <fieldset class="form-group">
+            <label>Email</label>
+            <input type="text" name="email" value="${st.getEmail()}" class="form-control" required/>
+        </fieldset>
+        <fieldset class="form-group">
+            <label>Mobile No.</label>
+            <input type="text" name="mobile_num" value="${st.getMobile_Num()}" class="form-control" required/>
+        </fieldset>
+        <fieldset class="form-group">
+            <label>Image</label>
+            <input type="file" name="photo" value="${st.getPhotopath()}" class="form-control" required/>
+        </fieldset>
+        <fieldset class="form-group">
+            <label>Username</label>
+            <input type="text" name="username" value="${st.getUsername()}" class="form-control" required/>
+        </fieldset>
+        <fieldset class="form-group">
+            <label>Password</label>
+            <input class="form-control" type="password" value="${st.getPassword()}" name="password" required/>
+        </fieldset>
+        <button type="submit" class="btn btn-success">Submit Details</button>
+    </form>
+</div>
 </body>
 </html>
